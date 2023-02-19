312dba4246bd0c1d1758e225295e12be481f797c
Logan Jerrow <Logan.Jerrow@Gmail.com>
2023-02-19 15:22:32 -0800

    added snake submodual for testing

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+pub mod acl; // Arithmetic Logic Unit
+pub mod architecture;
+pub mod memory;
+
+/*
+    Central Processing Unit (CPU)
+    Picture Processing Unit (PPU)
+    Random Access Memory (RAM) - 2 KiB (2048 bytes)
+    Cartridges
+    Gamepads
+*/
+
+pub mod apu; // Audio Processing Unit (APU)
+pub mod bus;
+pub mod cpu;
+pub mod gamepad;
+pub mod ppu;
+pub mod rom;
---
+++ src/ppu.rs
@@ -0,0 +1,3 @@
+// mod logic;
+// mod render;
+// mod scroll;
