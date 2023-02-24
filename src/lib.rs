7379e1c433b8a4b1847e421515a4bbebf5c9dd65
Logan Jerrow <Logan.Jerrow@Gmail.com>
2023-02-23 16:49:36 -0800

    test: :goal_net: Turn on more clippy lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+#![warn(
+    clippy::all,
+    clippy::complexity,
+    clippy::correctness,
+    clippy::perf,
+    clippy::style,
+    clippy::pedantic,
+    clippy::nursery,
+    clippy::unwrap_used,
+    clippy::expect_used
+)]
+#![allow(
+    dead_code,
+    unused,
+    clippy::missing_panics_doc,
+    clippy::missing_errors_doc
+)]
+
 pub mod acl; // Arithmetic Logic Unit
 pub mod architecture;
 pub mod memory;
