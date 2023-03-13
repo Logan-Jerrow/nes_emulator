036abe1e55c0c6df0eb0d8969cea7c03fd1f6175
Logan Jerrow <Logan.Jerrow@gmail.com>
2023-03-13 02:25:10 -0700

    docs: :memo: Add instruction comments to cpu::instruction mod

--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -1,6 +1,7 @@
 use super::{AddressingMode, CpuFlags, Memory, CPU};
 
 impl CPU {
+    /// ADC - Add with Carry
     #[allow(clippy::cast_possible_truncation)]
     pub fn adc(&mut self, mode: AddressingMode) {
         let addr = self.get_operand_address(mode);
@@ -33,6 +34,7 @@ impl CPU {
         self.update_zero_and_negative_flags(result);
     }
 
+    /// AND - Logical AND
     pub fn and(&mut self, mode: AddressingMode) {
         let addr = self.get_operand_address(mode);
         let value = self.mem_read(addr);
@@ -41,6 +43,51 @@ impl CPU {
         self.update_zero_and_negative_flags(self.register_a);
     }
 
+    /// ASL - Arithmetic Shift Left
+    ///
+    /// Shifts all bits left one position. 0 is shifted into bit 0 and the
+    /// original bit 7 is shifted into the Carry.
+    pub fn asl(&mut self, mode: AddressingMode) {
+        todo!()
+    }
+
+    /// BCC - Branch if Carry Clear
+    /// BCS - Branch if Carry Set
+    /// BEQ - Branch if Equal
+    /// BIT - Bit Test
+    /// BMI - Branch if Minus
+    /// BNE - Branch if Not Equal
+    /// BPL - Branch if Positive
+    /// BRK - Force Interrupt
+    /// BVC - Branch if Overflow Clear
+    /// BVS - Branch if Overflow Set
+    /// CLC - Clear Carry Flag
+    /// CLD - Clear Decimal Mode
+    /// CLI - Clear Interrupt Disable
+    /// CLV - Clear Overflow Flag
+    /// CMP - Compare
+    /// CPX - Compare X Register
+    /// CPY - Compare Y Register
+    /// DEC - Decrement Memory
+    /// DEX - Decrement X Register
+    /// DEY - Decrement Y Register
+    /// EOR - Exclusive OR
+    /// INC - Increment Memory
+    /// INX - Increment X Register
+    pub fn inx(&mut self) {
+        self.register_x = self.register_x.wrapping_add(1);
+        self.update_zero_and_negative_flags(self.register_x);
+    }
+
+    /// INY - Increment Y Register
+    pub fn iny(&mut self) {
+        self.register_y = self.register_y.wrapping_add(1);
+        self.update_zero_and_negative_flags(self.register_y);
+    }
+
+    /// JMP - Jump
+    /// JSR - Jump to Subroutine
+    /// LDA - Load Accumulator
     pub fn lda(&mut self, mode: AddressingMode) {
         let addr = self.get_operand_address(mode);
         let value = self.mem_read(addr);
@@ -49,6 +96,7 @@ impl CPU {
         self.update_zero_and_negative_flags(self.register_a);
     }
 
+    /// LDX - Load X Register
     pub fn ldx(&mut self, mode: AddressingMode) {
         let addr = self.get_operand_address(mode);
         let value = self.mem_read(addr);
@@ -57,6 +105,7 @@ impl CPU {
         self.update_zero_and_negative_flags(self.register_x);
     }
 
+    /// LDY - Load Y Register
     pub fn ldy(&mut self, mode: AddressingMode) {
         let addr = self.get_operand_address(mode);
         let value = self.mem_read(addr);
@@ -65,48 +114,62 @@ impl CPU {
         self.update_zero_and_negative_flags(self.register_y);
     }
 
+    /// LSR - Logical Shift Right
+    /// NOP - No Operation
+    /// ORA - Logical Inclusive OR
+    /// PHA - Push Accumulator
+    /// PHP - Push Processor Status
+    /// PLA - Pull Accumulator
+    /// PLP - Pull Processor Status
+    /// ROL - Rotate Left
+    /// ROR - Rotate Right
+    /// RTI - Return from Interrupt
+    /// RTS - Return from Subroutine
+    /// SBC - Subtract with Carry
+    /// SEC - Set Carry Flag
+    /// SED - Set Decimal Flag
+    /// SEI - Set Interrupt Disable
+    /// STA - Store Accumulator
     pub fn sta(&mut self, mode: AddressingMode) {
         let addr = self.get_operand_address(mode);
         self.mem_write(addr, self.register_a);
     }
 
+    /// STX - Store X Register
     pub fn stx(&mut self, mode: AddressingMode) {
         let addr = self.get_operand_address(mode);
         self.mem_write(addr, self.register_x);
     }
 
+    /// STY - Store Y Register
     pub fn sty(&mut self, mode: AddressingMode) {
         let addr = self.get_operand_address(mode);
         self.mem_write(addr, self.register_y);
     }
 
+    /// TAX - Transfer Accumulator to X
     pub fn tax(&mut self) {
         self.register_x = self.register_a;
         self.update_zero_and_negative_flags(self.register_x);
     }
 
-    pub fn txa(&mut self) {
-        self.register_a = self.register_x;
-        self.update_zero_and_negative_flags(self.register_a);
-    }
-
+    /// TAY - Transfer Accumulator to Y
     pub fn tay(&mut self) {
         self.register_y = self.register_a;
         self.update_zero_and_negative_flags(self.register_y);
     }
 
-    pub fn tya(&mut self) {
-        self.register_a = self.register_y;
+    /// TSX - Transfer Stack Pointer to X
+    /// TXA - Transfer X to Accumulator
+    pub fn txa(&mut self) {
+        self.register_a = self.register_x;
         self.update_zero_and_negative_flags(self.register_a);
     }
 
-    pub fn inx(&mut self) {
-        self.register_x = self.register_x.wrapping_add(1);
-        self.update_zero_and_negative_flags(self.register_x);
-    }
-
-    pub fn iny(&mut self) {
-        self.register_y = self.register_y.wrapping_add(1);
-        self.update_zero_and_negative_flags(self.register_y);
+    /// TXS - Transfer X to Stack Pointer
+    /// TYA - Transfer Y to Accumulator
+    pub fn tya(&mut self) {
+        self.register_a = self.register_y;
+        self.update_zero_and_negative_flags(self.register_a);
     }
 }
